5a2e9f2a5e68d7f30261c536d7a09d5149a31393
Austin Flores <ctrl4ltdeleteme@gmail.com>
2024-01-25 16:47:06 +0100

    First!

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
